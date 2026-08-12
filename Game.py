906d766f5502305e4928b8335966105dc455ec96
Krow Iroquois <krowiroquois@YorHa-Unit-9S-Remastered.local>
2026-08-12 13:35:04 -0700

    added method to class, tested out print statements

--- Game.py
+++ Game.py
@@ -3,3 +3,15 @@ class Game:
         self.name = name
         self.year = year
         self.price = price
+    
+    def set_price(self, amount):
+        if amount < 0:
+            print("Error: The price cannot be below zero!")
+        else:
+            self.price = amount
+
+game1 = Game("Sonic the Hedgehog", 2006, 15)
+game2 = Game("Bayonetta", 2010, 25)
+
+game1.set_price(-5)
+game2.set_price(35)
